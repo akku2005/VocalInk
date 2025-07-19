930dc0d3070513fa59df06c7d54fe7f4fc8d5423
gayu999Code <gayatri9mohite@gmail.com>
2025-07-20 01:09:34 +0530

    docs:Add the api-docs

---
+++ server/commitlint.config.js
@@ -0,0 +1,25 @@
+const { unlink } = require("./src/routes");
+
+module.exports = {
+    extends: ["@commitlint/config-conventional"], 
+    rules: {
+        "type-enum": [
+            2,
+            "always",
+            [
+                "feat",
+                "fix",
+                "docs",
+                "style",
+                "refactor",
+                "perf",
+                "test",
+                "build",
+                "ci",
+                "chore",
+                "revert"
+            ]
+        ],
+        "subject-case": [2, "always", "sentence-case"]
+    }
+}
--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 const dotenv = require('dotenv');
 const { connectDB } = require('./config/connectDB');
 const apiRouter = require('./routes');
+const swaggerDocument = require('../swagger.json');
+const swaggerUi = require('swagger-ui-express');
 
 // Optional: Use logger if available
 let logger = console;
@@ -22,6 +24,9 @@ connectDB().then(() => logger.info('Database connected')).catch(err => logger.er
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
+
+// API documentation
+app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerDocument));
 
 // API routes
 app.use('/api', apiRouter);
